b4f9a4449b9ac3e03807ef9e9877d5fb8ed2279d
ck <123@163.com>
2016-08-17 09:50:40 +0800

    0817

---
+++ exuetang/app/src/main/java/com/yutai/exuetang/view/activity/audio/MeAudioActivity.java
@@ -0,0 +1,15 @@
+package com.yutai.exuetang.view.activity.audio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yutai.exuetang.R;
+
+public class MeAudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_audio);
+    }
+}
